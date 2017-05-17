8309adefa7bd11ff83dfa342133b8b55a089df69
yangyangzhang <zyydeveloper@163.com>
2017-05-17 17:12:07 +0800

    Initial Commit

---
+++ AFNResearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNResearch
+//
+//  Created by Tech-zhangyangyang on 2017/5/17.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNResearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFNResearch
+//
+//  Created by Tech-zhangyangyang on 2017/5/17.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
